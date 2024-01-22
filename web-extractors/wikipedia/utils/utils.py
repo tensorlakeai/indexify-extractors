e984bbef8ed400bc8568462028a1f3aa81c353a6
Mohit Raghavendra <mohithmitra@gmail.com>
2024-01-22 15:54:32 -0500

    added labels to wiki extract

--- web-extractors/wikipedia/utils/utils.py
+++ web-extractors/wikipedia/utils/utils.py
@@ -1,11 +1,10 @@
-import json
 import os
 
 from typing import List
 
 from urllib.request import urlopen
 
-from indexify_extractor_sdk import Content, Feature
+from indexify_extractor_sdk import Content
 
 
 def save_html_pages(urls, path):
@@ -25,11 +24,13 @@ def parse_html_files(path: str) -> List[Content]:
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
         if file_path.endswith(".html"):
-            url_metadata = json.dumps({"url": filename})
+            # url_metadata = json.dumps({"url": filename})
+            labels = {"url": filename}
             with open(file_path, "r") as f:
                 document = Content.from_text(
                     text=f.read(),
-                    feature=Feature.metadata(value=url_metadata, name="url"),
+                    # feature=Feature.metadata(value=url_metadata, name="url"),
+                    labels=labels,
                 )
                 html_content.append(document)
 
