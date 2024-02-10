853431103c0b3dfc3abfd2402906b53113a91dc8
Jack Backes <jack@teamlumara.com>
2024-02-10 15:26:29 -0800

    formatting

--- indexify_extractor_sdk/packager.py
+++ indexify_extractor_sdk/packager.py
@@ -19,6 +19,7 @@ import sys
 import pathlib
 import concurrent.futures
 
+
 class DockerfileTemplate:
     """
     Manages the rendering of Dockerfile templates using Jinja2.
@@ -30,12 +31,20 @@ class DockerfileTemplate:
         configure: Prepares the template with specific configuration parameters.
         render: Renders the Dockerfile template with the provided configuration.
     """
+
     def __init__(self, template_path: str):
         self.template_path = template_path
         self.template = self._load_template()
         self.configuration_params = {}
 
-    def configure(self, extractor_path: "ExtractorPathWrapper", system_dependencies: List[str], python_dependencies: List[str], additional_pip_flags: str = "", dev: bool = False) -> "DockerfileTemplate":
+    def configure(
+        self,
+        extractor_path: "ExtractorPathWrapper",
+        system_dependencies: List[str],
+        python_dependencies: List[str],
+        additional_pip_flags: str = "",
+        dev: bool = False,
+    ) -> "DockerfileTemplate":
         self.configuration_params = {
             "extractor_path": extractor_path.format(),
             "module_name": extractor_path.module_name,
@@ -44,20 +53,22 @@ class DockerfileTemplate:
             "system_dependencies": system_dependencies,
             "python_dependencies": python_dependencies,
             "additional_pip_flags": additional_pip_flags,
-            "dev": dev
+            "dev": dev,
         }
         return self
 
     def _load_template(self) -> Template:
         try:
-            template_content = importlib.resources.read_text("dockerfiles", "Dockerfile.extractor")
+            template_content = importlib.resources.read_text(
+                "dockerfiles", "Dockerfile.extractor"
+            )
         except FileNotFoundError as e:
             try:
                 with open(self.template_path, "r") as f:
                     template_content = f.read()
             except FileNotFoundError as e:
                 raise FileNotFoundError(f"Failed to load template: {e}")
-    
+
         return Template(template_content)
 
     def render(self, **kwargs) -> str:
@@ -82,7 +93,7 @@ class ExtractorPathWrapper:
         self.module_name = module_name
         # i.e. "ColBERTv2Base"
         self.class_name = class_name
-    
+
     def validate(self) -> "ExtractorPathWrapper":
         # nothing to do for now
         return self
@@ -94,6 +105,7 @@ class ExtractorPathWrapper:
         # convert dashes to underscores
         file_name = self.module_name.replace("-", "_") + ".py"
         return file_name
+
 
 class ExtractorPackager:
     """
@@ -111,7 +123,15 @@ class ExtractorPackager:
         _add_dev_dependencies: Adds development dependencies to the tarball if applicable.
     """
 
-    def __init__(self, module_name: str, class_name: str, dockerfile_template_path: str = "../dockerfiles/Dockerfile.extractor", verbose: bool = False, dev: bool = False, gpu: bool = False):
+    def __init__(
+        self,
+        module_name: str,
+        class_name: str,
+        dockerfile_template_path: str = "../dockerfiles/Dockerfile.extractor",
+        verbose: bool = False,
+        dev: bool = False,
+        gpu: bool = False,
+    ):
         self.docker_client = DockerClient.from_env()
         self.logger = logging.getLogger(__name__)
         self.config = {
@@ -120,11 +140,13 @@ class ExtractorPackager:
             "dockerfile_template_path": dockerfile_template_path,
             "verbose": verbose,
             "dev": dev,
-            "gpu": gpu
+            "gpu": gpu,
         }
         self.logger.debug(f"Config: {self.config}")
-    
-        self.extractor_path = ExtractorPathWrapper(self.config["module_name"], self.config["class_name"]).validate()
+
+        self.extractor_path = ExtractorPathWrapper(
+            self.config["module_name"], self.config["class_name"]
+        ).validate()
 
         # try to load dynamically
         try:
@@ -135,10 +157,14 @@ class ExtractorPackager:
             module_path = cwd.joinpath(file_name)
             self.logger.info(f"Loading module from {module_path}")
 
-            module = DynamicModuleLoader.load_module_from_path(self.config["module_name"], module_path)
+            module = DynamicModuleLoader.load_module_from_path(
+                self.config["module_name"], module_path
+            )
             self.extractor_module = module
         except Exception as e:
-            self.logger.error(f"Failed to load extractor description for {self.extractor_path.format()} from {module_path}: {e}")
+            self.logger.error(
+                f"Failed to load extractor description for {self.extractor_path.format()} from {module_path}: {e}"
+            )
             raise
         # get the class from the module
         extractor_cls = getattr(module, self.config["class_name"])
@@ -150,20 +176,28 @@ class ExtractorPackager:
             "system_dependencies": extractor_cls.system_dependencies,
             "python_dependencies": extractor_cls.python_dependencies,
         }
-        assert self.extractor_description["name"] is not None, "Extractor.name must be defined"
-        assert self.extractor_description["version"] is not None, "Extractor.version must be defined"
-        
+        assert (
+            self.extractor_description["name"] is not None
+        ), "Extractor.name must be defined"
+        assert (
+            self.extractor_description["version"] is not None
+        ), "Extractor.version must be defined"
+
         # if dev, add the content of pyproject.toml - it's packaged in with resources
-        if self.config.get("dev", False):    
+        if self.config.get("dev", False):
             self.dev_files = {}
             self._collect_dev_files()
 
     def _collect_dev_files(self):
         # Initialize the structure for package files
         self.dev_files = {
-            "README.md": self._read_file_text(Path(__file__).parent.parent / "README.md"),
-            "pyproject.toml": self._read_file_text(Path(__file__).parent.parent / "pyproject.toml"),
-            "indexify_extractor_sdk": {"__files__": []}
+            "README.md": self._read_file_text(
+                Path(__file__).parent.parent / "README.md"
+            ),
+            "pyproject.toml": self._read_file_text(
+                Path(__file__).parent.parent / "pyproject.toml"
+            ),
+            "indexify_extractor_sdk": {"__files__": []},
         }
         # Recursively add files from the package directory
         self._add_package_files(Path(__file__).parent, "indexify_extractor_sdk")
@@ -203,7 +237,9 @@ class ExtractorPackager:
             raise
 
         try:
-            compressed_tar_stream = io.BytesIO(self._generate_compressed_tarball(dockerfile_content))
+            compressed_tar_stream = io.BytesIO(
+                self._generate_compressed_tarball(dockerfile_content)
+            )
         except Exception as e:
             self.logger.error(f"Failed to generate compressed tarball: {e}")
             raise
@@ -211,7 +247,6 @@ class ExtractorPackager:
         self.logger.info(f"Building image {self.extractor_description['name']}...")
         self._build_image(self.extractor_description["name"], compressed_tar_stream)
 
-    
     def _build_image(self, tag: str, fileobj: io.BytesIO):
         docker_client = docker.from_env()
         docker_stdout_handler = logging.StreamHandler(sys.stdout)
@@ -229,19 +264,41 @@ class ExtractorPackager:
                     encoding="gzip",
                     rm=True,
                     forcerm=True,
-                    pull=True
+                    pull=True,
                 )
             )
             self.logger.info(f"Successfully built image {image.tags[0]}")
-        
+
         except docker_err.BuildError as e:
-            self.logger.error(f"Failed to build image {self.extractor_description.get('name')}: {e}")
+            self.logger.error(
+                f"Failed to build image {self.extractor_description.get('name')}: {e}"
+            )
             # docker gives you the result_stream
             for chunk in e.build_log:
                 if "stream" in chunk:
-                    docker_stdout_handler.emit(logging.LogRecord("docker", logging.INFO, "docker", 0, chunk["stream"].strip(), None, None))
+                    docker_stdout_handler.emit(
+                        logging.LogRecord(
+                            "docker",
+                            logging.INFO,
+                            "docker",
+                            0,
+                            chunk["stream"].strip(),
+                            None,
+                            None,
+                        )
+                    )
                 elif "error" in chunk:
-                    docker_stderr_handler.emit(logging.LogRecord("docker", logging.ERROR, "docker", 0, chunk["error"].strip(), None, None))
+                    docker_stderr_handler.emit(
+                        logging.LogRecord(
+                            "docker",
+                            logging.ERROR,
+                            "docker",
+                            0,
+                            chunk["error"].strip(),
+                            None,
+                            None,
+                        )
+                    )
             raise
 
         except docker_err.APIError as e:
@@ -249,20 +306,34 @@ class ExtractorPackager:
             raise
 
     def _generate_dockerfile(self) -> str:
-        return DockerfileTemplate(self.config["dockerfile_template_path"]).configure(
-            extractor_path=self.extractor_path,
-            system_dependencies=" ".join(self.extractor_description["system_dependencies"]),
-            python_dependencies=" ".join(self.extractor_description["python_dependencies"]),
-            additional_pip_flags="" if self.config["gpu"] else "--extra-index-url https://download.pytorch.org/whl/cpu",
-            dev=self.config.get("dev", False)
-        ).render()
-    
+        return (
+            DockerfileTemplate(self.config["dockerfile_template_path"])
+            .configure(
+                extractor_path=self.extractor_path,
+                system_dependencies=" ".join(
+                    self.extractor_description["system_dependencies"]
+                ),
+                python_dependencies=" ".join(
+                    self.extractor_description["python_dependencies"]
+                ),
+                additional_pip_flags=(
+                    ""
+                    if self.config["gpu"]
+                    else "--extra-index-url https://download.pytorch.org/whl/cpu"
+                ),
+                dev=self.config.get("dev", False),
+            )
+            .render()
+        )
+
     def _generate_compressed_tarball(self, dockerfile_content: str) -> bytes:
         """
         Generates a tarball containing the Dockerfile and any additional files needed for the extractor.
         """
         dockerfile_tar_buffer = io.BytesIO()
-        with tarfile.open(fileobj=dockerfile_tar_buffer, mode="w|", format=tarfile.GNU_FORMAT) as tar:
+        with tarfile.open(
+            fileobj=dockerfile_tar_buffer, mode="w|", format=tarfile.GNU_FORMAT
+        ) as tar:
             dockerfile_info = tarfile.TarInfo("Dockerfile")
             dockerfile_info.mode = 0o755
             dockerfile_bytes = dockerfile_content.encode("utf-8")
@@ -272,14 +343,16 @@ class ExtractorPackager:
             # add the module that is at self.extractor_module to the tar using the module name
             # the path should be the project root in <module_name>.py
             module_info = tarfile.TarInfo(self.extractor_path.file_name())
-            module_bytes = pkg_resources.read_text(self.extractor_path.module_name, self.extractor_path.file_name()).encode("utf-8")
+            module_bytes = pkg_resources.read_text(
+                self.extractor_path.module_name, self.extractor_path.file_name()
+            ).encode("utf-8")
             module_info.size = len(module_bytes)
             module_info.mode = 0o644
             tar.addfile(module_info, io.BytesIO(module_bytes))
 
             if self.config.get("dev", False):
                 self._add_dev_dependencies(tar)
-        
+
         dockerfile_tar_buffer.seek(0)
         compressed_data_buffer = io.BytesIO()
         with gzip.GzipFile(fileobj=compressed_data_buffer, mode="wb") as gz:
@@ -295,7 +368,9 @@ class ExtractorPackager:
                 for f in content:
                     file_path = Path(base_path) / f
                     file_info = tarfile.TarInfo(name=str(Path(base_tar_path) / f))
-                    file_content = pkg_resources.read_text(base_path, str(f)).encode("utf-8")
+                    file_content = pkg_resources.read_text(base_path, str(f)).encode(
+                        "utf-8"
+                    )
                     file_info.size = len(file_content)
                     file_info.mode = 0o644  # Regular file permissions
                     tar.addfile(file_info, io.BytesIO(file_content))
@@ -322,11 +397,14 @@ class ExtractorPackager:
         readme_info.size = len(readme_content)
         readme_info.mode = 0o644
         tar.addfile(readme_info, io.BytesIO(readme_content))
-        
-        self._add_files_from_dir("indexify_extractor_sdk", "indexify_extractor_sdk", tar)
+
+        self._add_files_from_dir(
+            "indexify_extractor_sdk", "indexify_extractor_sdk", tar
+        )
 
         # print the directory structure of the tar
         tar.list(verbose=True)
+
 
 class DynamicModuleLoader:
     @staticmethod
@@ -347,17 +425,18 @@ class DynamicModuleLoader:
         spec.loader.exec_module(module)
         return module
 
+
 async def async_docker_build(client, **kwargs):
     """
     Asynchronously build a Docker image and stream build logs.
-    
+
     Args:
         client: Docker client instance.
         **kwargs: Keyword arguments for the Docker build command.
-    
+
     Returns:
         The Image object for the image that was built and a list of the build logs.
-    
+
     Raises:
         docker.errors.BuildError if there is an error during the build.
     """
@@ -376,36 +455,58 @@ async def async_docker_build(client, **kwargs):
 
         async for chunk in async_stream_logs(resp):
             build_logs.append(chunk)
-            if 'error' in chunk:
-                streaming_err_handler.emit(logging.LogRecord("docker", logging.ERROR, "docker", 0, chunk["error"].strip(), None, None))
-                raise docker.errors.BuildError(chunk['error'], build_logs)
-            if 'stream' in chunk:
-                streaming_log_handler.emit(logging.LogRecord("docker", logging.DEBUG, "docker", 0, chunk["stream"].strip(), None, None))
+            if "error" in chunk:
+                streaming_err_handler.emit(
+                    logging.LogRecord(
+                        "docker",
+                        logging.ERROR,
+                        "docker",
+                        0,
+                        chunk["error"].strip(),
+                        None,
+                        None,
+                    )
+                )
+                raise docker.errors.BuildError(chunk["error"], build_logs)
+            if "stream" in chunk:
+                streaming_log_handler.emit(
+                    logging.LogRecord(
+                        "docker",
+                        logging.DEBUG,
+                        "docker",
+                        0,
+                        chunk["stream"].strip(),
+                        None,
+                        None,
+                    )
+                )
                 match = re.search(
-                    r'(^Successfully built |sha256:)([0-9a-f]+)$',
-                    chunk['stream']
+                    r"(^Successfully built |sha256:)([0-9a-f]+)$", chunk["stream"]
                 )
                 if match:
                     image_id = match.group(2)
 
         if image_id:
-            image = await loop.run_in_executor(pool, lambda: client.images.get(image_id))
+            image = await loop.run_in_executor(
+                pool, lambda: client.images.get(image_id)
+            )
             return (image, build_logs)
         else:
             raise docker.errors.BuildError("Unknown error", build_logs)
 
+
 async def async_stream_logs(resp):
     """
     Asynchronously stream Docker build logs.
-    
+
     Args:
         resp: The response object from the Docker API build request.
-    
+
     Yields:
         Decoded JSON objects from the build logs.
     """
     for line in resp:
         # a line can have multiple json objects, connected with \r\n
-        for line in line.decode('utf-8').split("\r\n"):
+        for line in line.decode("utf-8").split("\r\n"):
             if line:
                 yield json.loads(line)
