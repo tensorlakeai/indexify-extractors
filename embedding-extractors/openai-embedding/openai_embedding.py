97010f8d52ba3b4012f902e7c653681f13de7a4a
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-13 14:38:05 -0800

    update OpenAIEmbeddingExtractor

--- embedding-extractors/openai-embedding/openai_embedding.py
+++ embedding-extractors/openai-embedding/openai_embedding.py
@@ -1,15 +1,14 @@
 from typing import List
-
-from indexify_extractor_sdk import EmbeddingSchema, ExtractorSchema
-from indexify_extractor_sdk.base_embedding import (
-    BaseEmbeddingExtractor,
-    EmbeddingInputParams,
-)
-
+from indexify_extractor_sdk.embedding.base_embedding import BaseEmbeddingExtractor
+from indexify_extractor_sdk.embedding.sentence_transformer import SentenceTransformersEmbedding
 from openai import OpenAI
 
-
 class OpenAIEmbeddingExtractor(BaseEmbeddingExtractor):
+    name = "openai-embedding-ada-002-extractor"
+    description = "OpenAI Embedding extractor"
+    python_dependencies = ["openai"]
+    system_dependencies = []
+
     def __init__(self):
         super(OpenAIEmbeddingExtractor, self).__init__(max_context_length=128)
         self.model_name = "text-embedding-ada-002"
@@ -19,17 +18,6 @@ class OpenAIEmbeddingExtractor(BaseEmbeddingExtractor):
         embeddings = self.client.embeddings.create(input=texts, model=self.model_name)
         return [data.embedding for data in embeddings.data]
 
-    def extract_query_embeddings(self, query: str) -> List[float]:
-        return (
-            self.client.embeddings.create(input=query, model=self.model_name)
-            .data[0]
-            .embedding
-        )
 
-    def schemas(self) -> ExtractorSchema:
-        input_params = EmbeddingInputParams()
-        return ExtractorSchema(
-            features={
-                "embedding": EmbeddingSchema(distance_metric="cosine", dim=1536)
-            },
-        )
+if __name__ == "__main__":
+    OpenAIEmbeddingExtractor().extract_sample_input()
