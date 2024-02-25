43a25c73331029a8034a93e8c8f645a582ef0de9
Mohit Raghavendra <mohithmitra@gmail.com>
2024-02-25 16:58:06 +0000

    notebook for text chunking

--- text/chunking/chunk_extractor.py
+++ text/chunking/chunk_extractor.py
@@ -1,23 +1,24 @@
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json
+from pydantic import BaseModel
 from langchain import text_splitter
+from langchain.docstore.document import Document
 from typing import Callable, List, Literal
 
 from indexify_extractor_sdk import Content, Extractor, Feature
 
 
-@dataclass_json
-@dataclass
-class ChunkExtractionInputParams:
+class ChunkExtractionInputParams(BaseModel):
     overlap: int = 0
     chunk_size: int = 100
-    text_splitter: Literal["char", "recursive"] = "recursive"
+    text_splitter: Literal["char", "recursive", "markdown", "html"] = "recursive"
+    headers_to_split_on: List[str] = []
 
 
 class ChunkExtractor(Extractor):
-    """
-    Extractor that chunks text into smaller pieces.
-    """
+    name = "tensorlake/chunk-extractor"
+    description = "Text Chunk Extractor"
+    python_dependencies = ["langchain", "lxml"]
+    system_dependencies = []
+
 
     def __init__(self):
         super().__init__()
@@ -31,9 +32,20 @@ class ChunkExtractor(Extractor):
         chunks = splitter(text)
         chunk_contents = []
         for chunk in chunks:
-            chunk_content = Content.from_text(
-                chunk, features=content.features, labels=content.labels
-            )
+            if type(chunk) == Document:
+                chunk_content = Content.from_text(
+                    chunk.page_content,
+                    features=content.features,
+                    labels=content.labels,
+                )
+                if chunk.metadata:
+                    chunk_content.features.append(Feature.metadata(chunk.metadata))
+            else:
+                chunk_content = Content.from_text(
+                    chunk,
+                    features=content.features,
+                    labels=content.labels
+                )
 
             chunk_contents.append(chunk_content)
 
@@ -52,6 +64,23 @@ class ChunkExtractor(Extractor):
                 chunk_size=input_params.chunk_size,
                 chunk_overlap=input_params.overlap,
                 separator="\n\n",
+            ).split_text
+        elif input_params.text_splitter == "markdown":
+            return text_splitter.MarkdownHeaderTextSplitter(
+                headers_to_split_on=[
+                                ("#", "Header 1"),
+                                ("##", "Header 2"),
+                                ("###", "Header 3"),
+                            ],
+            ).split_text
+        elif input_params.text_splitter == "html":
+            return text_splitter.HTMLHeaderTextSplitter(
+                headers_to_split_on = [
+                                ("h1", "Header 1"),
+                                ("h2", "Header 2"),
+                                ("h3", "Header 3"),
+                                ("h4", "Header 4"),
+                            ],
             ).split_text
 
     def sample_input(self) -> Content:
