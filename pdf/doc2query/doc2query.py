ad2bd4c04c1c90b93102c1cab2e69dcd7721ab91
Lucas Jackson <lucasjackson5815@gmail.com>
2024-03-11 17:12:44 -0700

    doc2query

---
+++ pdf/doc2query/doc2query.py
@@ -0,0 +1,60 @@
+from indexify_extractor_sdk import Content, Extractor, Feature
+from typing import List
+from docquery import document, pipeline
+from pydantic import BaseModel
+import tempfile
+
+
+class Doc2QueryConfig(BaseModel):
+    query: int = "What is the invoice total?"
+
+
+class Doc2Query(Extractor):
+    name = "tensorlake/doc2query-extractor"
+    description = "Doc2query"
+    input_mime_types = ["application/pdf", "image/jpeg", "image/png"]
+    system_dependencies = ["tesseract-ocr", "poppler-utils"]
+
+    def __init__(self):
+        super(Doc2Query, self).__init__()
+
+    def extract(self, content: Content, params: Doc2QueryConfig) -> Feature:
+        suffix = f'.{content.content_type.split("/")[-1]}'
+        with tempfile.NamedTemporaryFile(suffix=suffix, delete=True) as tmpfile:
+            # write bytes to temp file
+            tmpfile.write(content.data)
+            tmpfile.flush()
+
+            p = pipeline("document-question-answering")
+            doc = document.load_document(tmpfile.name)
+            result = p(question=params.query, **doc.context)[0]  # get first result
+
+        return Feature.metadata(
+            value={
+                "query": params.query,
+                "answer": result.get("answer"),
+                "score": result.get("score"),
+                "page": result.get("page"),
+            }
+        )
+
+    def sample_input(self) -> Content:
+        file_path = "invoice-example.pdf"
+
+        with open(file_path, "rb") as f:
+            data = f.read()
+
+        return Content(
+            data=data,
+            content_type="application/pdf",
+            features=[],
+        )
+
+
+if __name__ == "__main__":
+    contents = Doc2Query().extract_sample_input()
+    print(len(contents))
+    for content in contents:
+        print(len(content.features))
+        for feature in content.features:
+            print(feature.value)
