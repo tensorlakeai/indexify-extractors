0d07fe82501e80294530fe84680733cd16aebbb0
Lucas Jackson <lucasjackson5815@gmail.com>
2024-03-13 16:03:53 -0700

    UnstructuredIO PDF Extractor (#60)
    * unstructured extractor
    
    * update readme
    
    * update readme
    
    * update readme
    
    * update readme
    
    * update strategy for image
    
    * update names

---
+++ pdf/unstructuredio/README.md
@@ -0,0 +1,57 @@
+# Unstructured.io PDF Extractor
+This extractor uses [unstructured](https://github.com/Unstructured-IO/unstructured) to extract data from both pdf and image files. Unstructured will extract text from key elements along with important metadata like page number and type.
+
+### Output:
+text/plain for each element found
+
+### Input parameters
+strategy: Literal['hi_res', 'fast', 'ocr_only', 'auto'] = "fast"
+languages: Optional[Iterable[str]] = None
+
+### Strategies
+[Four strategies are available](https://github.com/Unstructured-IO/unstructured-api?tab=readme-ov-file#strategies) for processing PDF/Images files: hi_res, fast, ocr_only and auto. fast is the default strategy and works well for documents that do not have text embedded in images.
+
+hi_res is the better choice for PDFs that may have text within embedded images, or for achieving greater precision of element types in the response JSON.
+
+### Metadata:
+[Metadata extracted](https://unstructured-io.github.io/unstructured/metadata.html) from unstructured also applied to the content.
+
+detection_class_prob: Optional[float] = None # From unstructured-inference, hi-res strategy.
+coordinates: Coordinates
+last_modified: str
+filetype: str
+languages: List[str]
+page_number: int
+file_directory: str
+filename: str
+type: str
+
+### Additional Language Support
+This extractor depends on having [tesseract](https://github.com/tesseract-ocr/tesseract) installed.
+
+Install for Mac users
+```bash
+brew install tesseract
+```
+
+Install for Linux/Ubuntu
+```bash
+sudo apt install tesseract-ocr
+```
+
+To view currently installed language packs
+```bash
+tesseract --list-langs
+```
+
+To search for additional language packs:
+
+Linux
+```bash
+apt search tesseract-ocr
+```
+
+Install language packs on Mac
+```bash
+brew install tesseract-lang
+```
---
+++ pdf/unstructuredio/unstructured_pdf.py
@@ -0,0 +1,60 @@
+from typing import List, Literal, Optional, Iterable
+from pydantic import BaseModel
+from indexify_extractor_sdk import Extractor, Content, Feature
+import tempfile
+import mimetypes
+from unstructured.partition.pdf import partition_pdf
+from unstructured.partition.image import partition_image
+
+
+class UnstructuredIOConfig(BaseModel):
+    strategy: Literal['hi_res', 'fast', 'ocr_only', 'auto'] = "fast"
+    languages: Optional[Iterable[str]] = None
+
+
+class UnstructuredIOExtractor(Extractor):
+    name = "tensorlake/unstructuredio"
+    description = "This extractor uses unstructured.io to extract pieces of pdf document into separate plain text content data."
+    input_mime_types = ["application/pdf", "image/jpeg", "image.png"]
+    # language packs can be added to system dependencies
+    system_dependencies = ["tesseract-ocr"]
+
+    def __init__(self):
+        super(UnstructuredIOExtractor, self).__init__()
+
+    def extract(self, content: Content, params: UnstructuredIOConfig) -> List[Content]:
+        suffix = mimetypes.guess_extension(content.content_type)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmpfile:
+            tmpfile.write(content.data)
+            tmpfile.flush()
+
+            # detect content type
+            if suffix == ".pdf":
+                elements = partition_pdf(tmpfile.name, **dict(params))
+            else:
+                if params.strategy == "fast":
+                    print("fast is not supported on images, defaulting to hi_res")
+                    params.strategy = "hi_res"
+                elements = partition_image(tmpfile.name, **dict(params))
+
+            # extract data
+            new_content = []
+            for e in elements:
+                data = e.to_dict()
+                metadata = {**data.get("metadata"), "type": data.get("type")}
+                new_content.append(
+                    Content(
+                        content_type="text/plain",
+                        data=bytes(data.get("text"), "utf-8"),
+                        features=[Feature.metadata(value=metadata)],
+                    )
+                )
+            return new_content
+
+    def sample_input(self) -> Content:
+        f = open("sample.pdf", "rb")
+        return Content(content_type="application/pdf", data=f.read())
+
+
+if __name__ == "__main__":
+    UnstructuredIOExtractor().extract_sample_input()
