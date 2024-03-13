259b6151b038a27dc0e862729ea7a0d88acf5c91
Lucas Jackson <lucasjackson5815@gmail.com>
2024-03-12 19:38:17 -0700

    ocrmypdf extractor

---
+++ pdf/ocrmypdf/README.md
@@ -0,0 +1,41 @@
+# OCRMyPDF
+This extractor uses ocrmypdf to generate searchable PDF/A content from a regular PDF.
+
+
+### Input parameters
+
+- **`deskew`**: `Optional[bool] = None`  
+  Deskews crooked PDFs, helping in correcting the alignment of scanned documents.
+
+- **`language`**: `Optional[Iterable[str]] = None`  
+  Specifies the language(s) of the documents for improved OCR accuracy by using language-specific character recognition models.
+
+- **`remove_background`**: `Optional[bool] = None`  
+  Removes varied or distracting backgrounds from documents, enabling the OCR engine to focus more effectively on the text.
+
+- **`rotate_pages`**: `Optional[bool] = None`  
+  Automatically rotates pages to their correct orientation, which can significantly improve the readability and success rate of OCR processes.
+
+
+### Additional Language Support
+This extractor depends on having [tesseract](https://github.com/tesseract-ocr/tesseract) installed.
+
+Install for Mac users
+```bash
+brew install tesseract
+```
+
+Install for Linux/Ubuntu
+```bash
+sudo apt install tesseract-ocr
+```
+
+To view currently installed language packs
+```bash
+tesseract --list-langs
+```
+
+To search for additional language packs:
+```bash
+apt search tesseract-ocr
+```
---
+++ pdf/ocrmypdf/ocr_my_pdf.py
@@ -0,0 +1,40 @@
+from typing import List, Iterable, Optional
+from pydantic import BaseModel
+from indexify_extractor_sdk import Extractor, Content
+import tempfile
+import ocrmypdf
+
+class OCRMyPDFConfig(BaseModel):
+    deskew: Optional[bool] = None
+    language: Optional[Iterable[str]] = None
+    remove_background: Optional[bool] = None
+    rotate_pages: Optional[bool] = None
+    
+class OCRMyPDFExtractor(Extractor):
+    name = "tensorlake/ocrmypdf"
+    description = "OCRMyPDF for image based pdfs to editable searchable text based pdfs"
+    input_mime_types = ["application/pdf"]
+    # language packs can be added to system dependencies
+    system_dependencies = ["tesseract-ocr"]
+
+    def __init__(self):
+        super(OCRMyPDFExtractor, self).__init__()
+
+    def extract(self, content: Content, params: OCRMyPDFConfig) -> List[Content]:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as inputtmpfile:
+            inputtmpfile.write(content.data)
+            inputtmpfile.flush()
+
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as outtmpfile:
+                ocrmypdf.ocr(inputtmpfile.name, outtmpfile.name, **dict(params))
+                return [
+                    Content(content_type="application/pdf", data=outtmpfile.read())
+                ]
+            
+    def sample_input(self) -> Content:
+        f = open("image-based.pdf")
+        return Content(content_type="application/pdf", data=f.read())
+
+
+if __name__ == "__main__":
+    OCRMyPDFExtractor().extract_sample_input()
