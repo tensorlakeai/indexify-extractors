2b7eed2e7929d7152f6b432506ee8e7feba56c78
Lucas Jackson <lucasjackson5815@gmail.com>
2024-03-15 15:52:05 -0700

    use pixel format for bounding boxes

--- image/groundingdino/grounding_dino.py
+++ image/groundingdino/grounding_dino.py
@@ -4,12 +4,13 @@ from indexify_extractor_sdk import Extractor, Content, Feature
 import tempfile
 import mimetypes
 from groundingdino.util.inference import load_model, load_image, predict
+from PIL import Image
 
 
 class GroundingDinoConfig(BaseModel):
     prompt: str = "dog"
-    box_threshold:float = 0.35
-    text_threshold:float = 0.25
+    box_threshold: float = 0.35
+    text_threshold: float = 0.25
 
 
 class GroundingDinoExtractor(Extractor):
@@ -23,6 +24,23 @@ class GroundingDinoExtractor(Extractor):
         self.model = load_model(
             "GroundingDINO_SwinT_OGC.py", "groundingdino_swint_ogc.pth"
         )
+
+    def scale_bounding_box_to_pixels(self, box, img_width, img_height):
+        x_center, y_center, width, height = box
+
+        # Calculate pixel values
+        box_width = width * img_width
+        box_height = height * img_height
+        left = (x_center * img_width) - (box_width / 2)
+        top = (y_center * img_height) - (box_height / 2)
+        right = left + box_width
+        bottom = top + box_height
+        return [int(v) for v in [left, top, right, bottom]]
+
+    def get_image_size(self, image_path):
+        with Image.open(image_path) as img:
+            width, height = img.size
+        return width, height
 
     def extract(self, content: Content, params: GroundingDinoConfig) -> List[Feature]:
         suffix = mimetypes.guess_extension(content.content_type)
@@ -40,13 +58,15 @@ class GroundingDinoExtractor(Extractor):
                 text_threshold=params.text_threshold,
                 device="cpu",
             )
-
+            width, height = self.get_image_size(inputtmpfile.name)
             metadata = []
             for box, logit, phrase in zip(boxes, logits, phrases):
                 metadata.append(
                     Feature.metadata(
                         value={
-                            "boundingbox": box.tolist(),
+                            "boundingbox": self.scale_bounding_box_to_pixels(
+                                box.tolist(), width, height
+                            ),
                             "score": logit.item(),
                             "phrase": phrase,
                         }
