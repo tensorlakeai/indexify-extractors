c2993cb0cb0844940920174d023bbe31800e8a76
Lucas Jackson <lucasjackson5815@gmail.com>
2024-04-04 11:38:20 -0700

    remove print

--- extractor-sdk/indexify_extractor_sdk/server.py
+++ extractor-sdk/indexify_extractor_sdk/server.py
@@ -68,7 +68,6 @@ class ServerWithNoSigHandler(uvicorn.Server):
 def http_server(server_router: ServerRouter, listen_addr: str) -> uvicorn.Server:
     print("starting extraction server endpoint")
     host, port = listen_addr.split(":")
-    print("host", host, "port", port)
     app = FastAPI()
     app.include_router(server_router.router)
     config = uvicorn.Config(
