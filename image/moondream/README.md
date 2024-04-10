b899d4ee6c5c97599d9ec272d2123b7ef3472d6e
ryland <ryland@temporal.io>
2024-04-10 13:03:47 -0700

    add moondream2 extractor

---
+++ image/moondream/README.md
@@ -0,0 +1,14 @@
+# Moondream Extractor
+
+"A tiny vision language model that kicks ass and runs anywhere"
+
+moondream2 is a 1.86B parameter model initialized with weights from SigLIP and Phi 1.5.
+
+[GitHub](https://github.com/vikhyat/moondream)
+[Hugging Face](https://github.com/vikhyat/moondream)
+
+Supported image types: ["image/bmp", "image/gif", "image/jpeg", "image/png", "image/tiff"]
+
+### Test locally
+
+`python moondream_extractor.py`
---
+++ image/moondream/moondream_extractor.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel
+
+from typing import List
+import torch
+
+from indexify_extractor_sdk import Extractor, Content, Feature
+from indexify_extractor_sdk.base_extractor import Content
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import PIL.Image as Image
+import io
+import json
+
+class MoondreamConfig(BaseModel):
+    prompt: str = "Describe this image."
+
+class MoondreamExtractor(Extractor):
+    name = "tensorlake/moondream"
+    description = "This Extractor uses Moondream which is tiny LLM that can answer questions about images"
+
+    input_mime_types = ["image/bmp", "image/gif", "image/jpeg", "image/png", "image/tiff"]
+
+    def __init__(self):
+        super(MoondreamExtractor).__init__()
+
+        model_id = "vikhyatk/moondream2"
+        revision = "2024-04-02"
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id, trust_remote_code=True, revision=revision
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+    def extract(self, content: Content, params: MoondreamConfig) -> List[Content]:
+        image = Image.open(io.BytesIO(content.data))
+        enc_image = self.model.encode_image(image)
+        answer = self.model.answer_question(enc_image, params.prompt, self.tokenizer)
+        return answer
+
+    def sample_input(self) -> Content:
+        return self.sample_jpg()
+
+if __name__ == "__main__":
+    extractor = MoondreamExtractor()
+    input = extractor.sample_input()
+    results = extractor.extract(input, MoondreamConfig())
+    print(results)
