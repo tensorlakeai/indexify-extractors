cbd8fc51e632ab54eea090fc3eaa3ebd8209cde9
rishiraj <racharya.in@gmail.com>
2024-04-26 17:14:57 +0530

    normalize embeddings and add gpu support

--- embedding/clip_embedding/openai_clip_extractor.py
+++ embedding/clip_embedding/openai_clip_extractor.py
@@ -1,5 +1,6 @@
 from typing import List, Union
 from indexify_extractor_sdk import Content, Extractor, Feature
+import torch
 import clip
 from PIL import Image
 from io import BytesIO
@@ -12,19 +13,25 @@ class ClipEmbeddingExtractor(Extractor):
 
     def __init__(self):
         super(ClipEmbeddingExtractor, self).__init__()
-        self._model, self._preprocess = clip.load("ViT-B/32", jit=True)
+        self._device = "cuda" if torch.cuda.is_available() else "cpu"
+        self._model, self._preprocess = clip.load("ViT-B/32", device=self._device)
 
     def extract(self, content: Content, params = None) -> Union[List[Feature], List[Content]]:
         if "image" in content.content_type:
             img = Image.open(BytesIO(content.data))
-            image = self._preprocess(img).unsqueeze(0)
-            embedding = self._model.encode_image(image)
-            embedding = embedding.tolist()[0]
+            image = self._preprocess(img).unsqueeze(0).to(self._device)
+            with torch.no_grad():
+                image_features = self._model.encode_image(image)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            embedding = image_features.numpy()[0]
             return [Feature.embedding(values=embedding)]
         elif "text" in content.content_type:
-            text = content.data.decode("utf-8")
-            embedding = self._model.encode_text(clip.tokenize(text))
-            embedding = embedding.tolist()[0]
+            txt = content.data.decode("utf-8")
+            text = clip.tokenize(txt).to(self._device)
+            with torch.no_grad():
+                text_features = self._model.encode_text(text)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            embedding = text_features.numpy()[0]
             return [Feature.embedding(values=embedding)]
         else:
             raise ValueError("Unsupported Content Type")
