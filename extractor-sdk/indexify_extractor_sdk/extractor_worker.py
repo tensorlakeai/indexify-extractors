583ea2165ab9b9e3f9f80cb8f55c2c33de52455f
Edwin Kys <itsedwinkys@gmail.com>
2024-04-29 11:52:55 -0500

    feat: initial impl of register executor multi extractors

--- extractor-sdk/indexify_extractor_sdk/extractor_worker.py
+++ extractor-sdk/indexify_extractor_sdk/extractor_worker.py
@@ -1,43 +1,110 @@
 from typing import List, Union, Dict
-from .base_extractor import Content, ExtractorWrapper, Feature
+from .base_extractor import Content, ExtractorWrapper, Feature, ExtractorDescription
 from pydantic import Json, BaseModel
 import concurrent
+from .downloader import get_db_path
+import sqlite3
+import os
 
 
 class ExtractorModule(BaseModel):
     module_name: str
     class_name: str
 
+# str here is ExtractorDescription.name
+extractor_wrapper_map: Dict[str, ExtractorWrapper] = {}
 
-def create_extractor_wrapper(extractor_module: ExtractorModule) -> ExtractorWrapper:
-    print("creating extractor wrapper")
-    global extractor_wrapper
-    extractor_wrapper = ExtractorWrapper(
-        extractor_module.module_name, extractor_module.class_name
-    )
+def create_extractor_wrapper_map():
+    global extractor_wrapper_map
+    print("creating extractor wrappers")
+
+    conn = sqlite3.connect(get_db_path())
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM extractors")
+    records = cur.fetchall()
+
+    # Return error if no extractors are found
+    if len(records) == 0:
+        conn.close()
+        raise ValueError(
+            "No extractors found in the database.",
+            "Please run the downloader to download extractors."
+        )
+
+    for row in records:
+        print(f"adding extractor: {row[1]}")
+        module, cls = row[0].split(":")
+        extractor_wrapper = ExtractorWrapper(module, cls)
+        extractor_wrapper_map[row[1]] = extractor_wrapper
+
+    conn.close()
 
 
-def create_executor(extractor_module: ExtractorModule, workers: int):
+def create_executor(workers: int):
     print("creating executor")
     return concurrent.futures.ProcessPoolExecutor(
-        initializer=create_extractor_wrapper,
+        initializer=create_extractor_wrapper_map,
         max_workers=workers,
-        initargs=(extractor_module,),
     )
 
 
-def _extract_content(task_content_map: Dict[str, Content], task_params_map: Dict[str, Json]) -> List[Content]:
-    return extractor_wrapper.extract_batch(task_content_map, task_params_map)
+def _extract_content(
+    task_content_map: Dict[str, Content],
+    task_params_map: Dict[str, Json],
+    task_extractor_map: Dict[str, str],
+) -> Dict[str, Union[List[Feature], List[Content]]]:
+    result = {}
+
+    for extractor_name, extractor_wrapper in extractor_wrapper_map.items():
+        # Get task IDs using the extractor
+        task_ids = [
+            task_id
+            for task_id, extractor_name in task_extractor_map.items()
+            if extractor_name == extractor_name
+        ]
+
+        # Filter task contents and params using the task IDs
+        task_contents = {}
+        for task_id in task_ids:
+            task_contents[task_id] = task_content_map[task_id]
+
+        params =  {}
+        inner = task_params_map["dummy_task_id"]
+        for task_id in task_ids:
+            params[task_id] = inner[task_id]
+        task_params = {"dummy_task_id": params}
+
+        # Extract content using the right extractor
+        extracted = extractor_wrapper.extract_batch(task_contents, task_params)
+
+        # Add the extracted data to the result
+        for task_id, extracted_data in extracted.items():
+            result[task_id] = extracted_data
+
+    return result
 
 
-def _describe():
-    return extractor_wrapper.describe()
+def _describe() -> List[ExtractorDescription]:
+    return [
+        wrapper.describe()
+        for _, wrapper in extractor_wrapper_map.items()
+    ]
 
 
 async def extract_content(
-    loop, executor, content_list: Dict[str, Content], params: Json
+    loop, 
+    executor, 
+    content_list: Dict[str, Content], 
+    params: Json,
+    extractors: Dict[str, str] # task ID -> extractor name
 ) -> Dict[str, List[Union[Feature, Content]]]:
-    return await loop.run_in_executor(executor, _extract_content, content_list, {"dummy_task_id": params})
+    return await loop.run_in_executor(
+        executor, 
+        _extract_content, 
+        content_list, 
+        {"dummy_task_id": params},
+        extractors
+    )
 
 
 async def describe(loop, executor):
--- extractor-sdk/indexify_extractor_sdk/main.py
+++ extractor-sdk/indexify_extractor_sdk/main.py
@@ -6,6 +6,7 @@ import logging
 import os
 from .list_extractors import list_extractors
 import sys
+from .downloader import get_db_path
 
 import multiprocessing
 from typing_extensions import Annotated
@@ -59,13 +60,8 @@ def run_local(
     indexify_extractor.local(extractor, text, file)
 
 
-@typer_app.command(help="Joins the extractor to the coordinator server")
+@typer_app.command(help="Joins the executor to the coordinator server")
 def join_server(
-    # optional, default to $EXTRACTOR_PATH if not provided. If $EXTRACTOR_PATH is not set, it will raise an error.
-    extractor: str = typer.Argument(
-        None,
-        help="The extractor name in the format 'module_name:class_name'. For example, 'mock_extractor:MockExtractor'.",
-    ),
     coordinator_addr: str = "localhost:8950",
     ingestion_addr: str = "localhost:8900",
     listen_port: int = typer.Option(
@@ -82,19 +78,17 @@ def join_server(
     config_path: Optional[str] = typer.Option(None, help="Path to the TLS configuration file")
 ):
     print_version()
-    if not extractor:
-        extractor = os.environ.get("EXTRACTOR_PATH")
-        print(f"Using extractor path from $EXTRACTOR_PATH: {extractor}")
-        assert extractor, "Extractor path not provided and $EXTRACTOR_PATH not set."
-        
-    extractor_directory = os.path.join(os.path.expanduser("~"), ".indexify-extractors", os.path.basename(extractor.split(".")[0]))
-    sys.path.append(extractor_directory)
+
+    # Check if any extractors are downloaded.
+    path = os.path.join(os.path.expanduser("~"), ".indexify-extractors")
+    if not os.path.isdir(path):
+        print(path)
+        raise Exception("No extractors found. Download extractors using the downloader.")
     
     print("workers ", workers)
     print("config path provided ", config_path)
 
     indexify_extractor.join(
-        extractor=extractor,
         workers=workers,
         coordinator_addr=coordinator_addr,
         ingestion_addr=ingestion_addr,
