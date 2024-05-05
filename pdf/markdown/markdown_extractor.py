d2191903362c43cf8f70547b36453f3107050638
rishiraj <racharya.in@gmail.com>
2024-05-05 19:12:01 +0530

    complete markdown extractor

--- pdf/markdown/markdown_extractor.py
+++ pdf/markdown/markdown_extractor.py
@@ -2,7 +2,7 @@ from typing import List, Union
 import io
 from marker.convert import convert_single_pdf
 from marker.models import load_all_models
-import json
+import tempfile
 from indexify_extractor_sdk import Content, Extractor, Feature
 
 class MarkdownExtractor(Extractor):
@@ -17,10 +17,14 @@ class MarkdownExtractor(Extractor):
     def extract(self, content: Content, params = None) -> List[Union[Feature, Content]]:
         contents = []
         model_lst = load_all_models()
-        full_text, out_meta = convert_single_pdf(io.BytesIO(content.data), model_lst, max_pages=None, parallel_factor=1)
-        
-        feature = Feature.metadata(value=out_meta, name="text")
-        contents.append(Content.from_text(full_text, features=[feature]))
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as inputtmpfile:
+            inputtmpfile.write(content.data)
+            inputtmpfile.flush()
+
+            full_text, out_meta = convert_single_pdf(inputtmpfile.name, model_lst, max_pages=None, parallel_factor=1)
+            
+            feature = Feature.metadata(value=out_meta, name="text")
+            contents.append(Content.from_text(full_text, features=[feature]))
 
         return contents
 
@@ -28,7 +32,7 @@ class MarkdownExtractor(Extractor):
         return self.sample_scientific_pdf()
 
 if __name__ == "__main__":
-    f = open("2310.16944.pdf", "rb")
+    f = open("test.pdf", "rb")
     pdf_data = Content(content_type="application/pdf", data=f.read())
     extractor = MarkdownExtractor()
     results = extractor.extract(pdf_data)
