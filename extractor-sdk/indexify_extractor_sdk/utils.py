ae334ca9ee60cded02c731f6c1300625e0049ef3
Lucas Jackson <lucasjackson5815@gmail.com>
2024-05-09 15:52:02 -0700

    add request to track tensorlake extractor downloads

--- extractor-sdk/indexify_extractor_sdk/utils.py
+++ extractor-sdk/indexify_extractor_sdk/utils.py
@@ -1,4 +1,5 @@
 from itertools import islice
+import requests
 
 
 # https://docs.python.org/3/library/itertools.html#itertools.batched
@@ -9,3 +10,13 @@ def batched(iterable, n):
     it = iter(iterable)
     while batch := tuple(islice(it, n)):
         yield batch
+
+
+def log_event(event, value):
+    try:
+        requests.post(
+            "https://www.tensorlake.ai/api/analytics", json={"event": event, "value": value}
+        )
+    except Exception as e:
+        # fail silently
+        pass
