15158bcd5614ae44fc1fc21859e78949f0481fbb
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2024-05-28 13:00:18 +0530

    Update pdf_extractor.py

--- pdf/pdf-extractor/pdf_extractor.py
+++ pdf/pdf-extractor/pdf_extractor.py
@@ -1,9 +1,13 @@
-from typing import List, Union
+from typing import List, Union, Optional
 import json
 from indexify_extractor_sdk import Content, Extractor, Feature
+from pydantic import BaseModel, Field
 from .utils.tt_module import get_tables
 import fitz
 import tempfile
+
+class PDFExtractorConfig(BaseModel):
+    output_types: List[str] = Field(default_factory=lambda: ["text", "image", "table"])
 
 class PDFExtractor(Extractor):
     name = "tensorlake/pdf-extractor"
@@ -12,36 +16,39 @@ class PDFExtractor(Extractor):
     input_mime_types = ["application/pdf"]
 
     def __init__(self):
-        super().__init__()
+        super(PDFExtractor, self).__init__()
 
-    def extract(self, content: Content, params = None) -> List[Union[Feature, Content]]:
+    def extract(self, content: Content, params: PDFExtractorConfig) -> List[Union[Feature, Content]]:
         contents = []
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as inputtmpfile:
             inputtmpfile.write(content.data)
             inputtmpfile.flush()
-
             doc = fitz.open(inputtmpfile.name)
+
             for i in range(len(doc)):
                 page = doc[i]
-                page_text = page.get_text()
-                image_list = page.get_images()
-                feature = Feature.metadata(value={"type": "text", "page": i+1})
-                contents.append(Content.from_text(page_text, features=[feature]))
 
-                for img in image_list:
-                    xref = img[0]
-                    pix = fitz.Pixmap(doc, xref)
-                    if not pix.colorspace.name in (fitz.csGRAY.name, fitz.csRGB.name):
-                        pix = fitz.Pixmap(fitz.csRGB, pix)
-                    feature = Feature.metadata({"type": "image", "page": i+1})
-                    contents.append(Content(content_type="image/png", data=pix.tobytes(), features=[feature]))
+                if "text" in params.output_types:
+                    page_text = page.get_text()
+                    feature = Feature.metadata(value={"type": "text", "page": i+1})
+                    contents.append(Content.from_text(page_text, features=[feature]))
 
-        tables = get_tables(content.data)
+                if "image" in params.output_types:
+                    image_list = page.get_images()
+                    for img in image_list:
+                        xref = img[0]
+                        pix = fitz.Pixmap(doc, xref)
+                        if not pix.colorspace.name in (fitz.csGRAY.name, fitz.csRGB.name):
+                            pix = fitz.Pixmap(fitz.csRGB, pix)
+                        feature = Feature.metadata({"type": "image", "page": i+1})
+                        contents.append(Content(content_type="image/png", data=pix.tobytes(), features=[feature]))
 
-        for page, content in tables.items():
-            feature = Feature.metadata({"type": "table", "page": int(page)})
-            contents.append(Content(content_type="application/json", data=json.dumps(content), features=[feature]))
-        
+            if "table" in params.output_types:
+                tables = get_tables(content.data)
+                for page, content in tables.items():
+                    feature = Feature.metadata({"type": "table", "page": int(page)})
+                    contents.append(Content(content_type="application/json", data=json.dumps(content), features=[feature]))
+
         return contents
 
     def sample_input(self) -> Content:
@@ -51,5 +58,6 @@ if __name__ == "__main__":
     f = open("2310.16944.pdf", "rb")
     pdf_data = Content(content_type="application/pdf", data=f.read())
     extractor = PDFExtractor()
-    results = extractor.extract(pdf_data)
+    params = PDFExtractorConfig(output_types=["text", "table"])
+    results = extractor.extract(pdf_data, params)
     print(results)
