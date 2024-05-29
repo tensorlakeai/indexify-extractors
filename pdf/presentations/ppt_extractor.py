21e8c21109930fdd72513ee14e685260d4cc58a2
rishiraj <racharya.in@gmail.com>
2024-05-30 04:12:48 +0530

    add temporary fix to ppt extractor

--- pdf/presentations/ppt_extractor.py
+++ pdf/presentations/ppt_extractor.py
@@ -2,10 +2,11 @@ from typing import List, Union, Optional
 from pydantic import BaseModel, Field
 from indexify_extractor_sdk import Content, Extractor, Feature
 from pptx import Presentation
+import requests
 import tempfile
 
 class PPTExtractorConfig(BaseModel):
-    output_types: List[str] = Field(default_factory=lambda: ["text", "image", "table"])
+    output_types: List[str] = Field(default_factory=lambda: ["text", "table"])
 
 class PPTExtractor(Extractor):
     name = "tensorlake/ppt"
@@ -46,7 +47,12 @@ class PPTExtractor(Extractor):
         return contents
 
     def sample_input(self) -> Content:
-        return self.sample_presentation()
+        req = requests.get("https://raw.githubusercontent.com/tensorlakeai/indexify/main/docs/docs/files/test.pptx")
+        with open('test.pptx','wb') as f:
+            f.write(req.content)
+        with open('test.pptx','rb') as f:
+            ppt_data = f.read()
+        return Content(content_type="application/vnd.openxmlformats-officedocument.presentationml.presentation", data=ppt_data)
 
 if __name__ == "__main__":
     filepath = "test.pptx"
