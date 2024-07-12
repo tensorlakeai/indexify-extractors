cdc4febf5e8a36107ee59c6496d0bedb6ce19f39
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-07-11 17:44:54 -0700

    update moondream extractor to be batched

--- image/moondream/moondream_extractor.py
+++ image/moondream/moondream_extractor.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from typing import List
+from typing import List, Dict, Union
 import torch
 
 from accelerate import Accelerator
@@ -9,7 +9,6 @@ from indexify_extractor_sdk.base_extractor import Content
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import PIL.Image as Image
 import io
-import json
 
 class MoondreamConfig(BaseModel):
     prompt: str = "Describe this image."
@@ -42,11 +41,28 @@ class MoondreamExtractor(Extractor):
         self.model.eval()
         self.tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
 
-    def extract(self, content: Content, params: MoondreamConfig) -> List[Content]:
-        image = Image.open(io.BytesIO(content.data))
-        enc_image = self.model.encode_image(image)
-        answer = self.model.answer_question(enc_image, params.prompt, self.tokenizer)
-        return [Content.from_text(answer)]
+    def extract_batch(
+        self, content_list: Dict[str, Content], input_params: Dict[str, MoondreamConfig]
+    ) -> Dict[str, List[Union[Feature, Content]]]:
+        images = []
+        prompts = []
+        tasks = []
+        for (task_id, content) in content_list.items():
+            image = Image.open(io.BytesIO(content.data))
+            config = input_params.get(task_id, MoondreamConfig())
+            images.append(image)
+            prompts.append(config.prompt)
+            tasks.append(task_id)
+        answers = self.model.batch_answer(images, prompts, self.tokenizer)
+        results = {}
+        for (task_id, answer) in zip(tasks, answers):
+            results[task_id] = [Content.from_text(answer)]
+        return results
+
+    def extract(
+        self, content: Content, params: MoondreamConfig = None
+    ) -> List[Union[Feature, Content]]:
+        raise NotImplementedError()
 
     def sample_input(self) -> Content:
         return self.sample_jpg()
@@ -54,5 +70,5 @@ class MoondreamExtractor(Extractor):
 if __name__ == "__main__":
     extractor = MoondreamExtractor()
     input = extractor.sample_input()
-    results = extractor.extract(input, MoondreamConfig())
+    results = extractor.extract_batch({"task_id": input}, {"task_id": MoondreamConfig()})
     print(results)
