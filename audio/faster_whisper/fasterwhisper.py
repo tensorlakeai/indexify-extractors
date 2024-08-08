b6a354b5ae717f3890fb6208b913248807499987
sadath-12 <sadathsadu2002@gmail.com>
2024-08-08 10:57:32 +0530

    fix: faster_whisper
    Signed-off-by: sadath-12 <sadathsadu2002@gmail.com>

--- audio/faster_whisper/fasterwhisper.py
+++ audio/faster_whisper/fasterwhisper.py
@@ -1,18 +1,20 @@
 from pydantic import BaseModel
 from typing import List
-from indexify_extractor_sdk import Extractor, Content, Feature
+from indexify_extractor_sdk import Extractor, Content
 import json
-import subprocess
-import re
-import os
 import tempfile
-import argparse
-from transformers.pipelines.audio_utils import ffmpeg_read
-import torch
-from faster_whisper import WhisperModel,decode_audio
+from faster_whisper import WhisperModel
 
 class InputParams(BaseModel):
     model: str = "small"
+
+def save_bytes_to_wav(bytes_payload):
+    with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as wav_file:
+        wav_file.write(bytes_payload)
+        temp_file_name = wav_file.name
+    
+    return temp_file_name
+
 
 class FasterWhisper(Extractor):
     name = "tensorflake/fasterWhisper"
@@ -24,35 +26,14 @@ class FasterWhisper(Extractor):
         super().__init__()
 
     def extract(self, content: Content, params: InputParams) -> List[Content]:
-        # Create a temporary file to hold the audio data
-        with tempfile.NamedTemporaryFile() as fp:
-            fp.write(content.data)  # Write the audio bytes to the file
-            file = open(fp.name, "rb").read() # Store the name for later use
-        # Use ffmpeg_read to convert the audio file to a float32 tensor
-        inputs = ffmpeg_read(file, 16000)
-
-        # Convert the input to a Torch tensor and ensure it's float32
-        diarizer_inputs = torch.from_numpy(inputs).float()
-        print("Diarizer inputs type (before unsqueeze):", diarizer_inputs.dtype)
-        print("Diarizer inputs type (before unsqueeze): dim", diarizer_inputs.shape)
-        # diarizer_inputs = diarizer_inputs.unsqueeze(0)  # Add batch dimension
-        # Load the Whisper model
-        print("Diarizer inputs type (after unsqueeze):", diarizer_inputs.dtype)
-        print("Diarizer inputs type (after unsqueeze): dim", diarizer_inputs.shape)
+        wavFile = save_bytes_to_wav(content.data)
+    
         model = WhisperModel(params.model, device="cpu", compute_type="int8")
 
-        # Transcribe the audio using the model
-        right = decode_audio("output.wav", split_stereo=False)
+        segments, info = model.transcribe(wavFile, beam_size=5)
 
-        print("right dtype",right.dtype)
-        print("right shape",right.shape)
-
-        segments, info = model.transcribe(diarizer_inputs, beam_size=5)
-
-        # Optionally, parse and convert segments to JSON or any other format
         entries = []
         for segment in segments:
-            print(segment.text.strip())
             entries.append({
                 "timestamp": {"start": segment.start, "end": segment.end},
                 "text": segment.text.strip()
